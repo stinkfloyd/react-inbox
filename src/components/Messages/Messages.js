2f742b0c77a847f547dff9c56a1972d6b5339d78
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-28 08:31:12 -0700

    "Star function working"

---
+++ src/components/Messages/Messages.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Message from '../Message/Message.js'
+
+const Messages = ({ messages, toggleSelect, toggleStar }) => {
+    const messageComponents = messages.map(message => (
+        <Message
+            key={message.id}
+            message={message}
+            toggleSelect={toggleSelect}
+            toggleStar={toggleStar}
+        />
+    ))
+
+    return (
+        <div>
+            {messageComponents}
+        </div>
+    )
+}
+
+export default Messages
--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Toolbar from '../toolbar/toolbar';
-import MessageList from '../messageList/messageList';
+import Messages from '../Messages/Messages';
 
 export default class App extends Component {
   constructor(props) {
@@ -11,30 +11,69 @@ export default class App extends Component {
     }
   }
 
-  starCallback = (value, id) => {
+  toggleProperty(message, property) {
+    const index = this.state.messages.indexOf(message)
+    this.setState({
+      messages: [
+        ...this.state.messages.slice(0, index),
+        { ...message, [property]: !message[property] },
+        ...this.state.messages.slice(index + 1),
+      ]
+    })
+  }
 
+  toggleSelect = async (message) => {
+    this.toggleProperty(message, 'selected')
+  }
+
+  toggleStar = async (message) => {
+    await this.updateMessages({
+      "messageIds": [message.id],
+      "command": "star",
+      "star": message.starred
+    })
+
+    this.toggleProperty(message, 'starred')
+  }
+
+  async updateMessages(payload) {
+    await this.request(process.env.REACT_APP_API_URL, 'PATCH', payload)
   }
 
   getMessages = async () => {
     const response = await fetch(`${process.env.REACT_APP_API_URL}`)
     let resJson = await response.json()
-
-
     this.setState({
       ...this.state,
       messages: resJson
     })
   }
 
+  request = async (path, method = 'GET', body = null) => {
+    if (body) body = JSON.stringify(body)
+    return await fetch(process.env.REACT_APP_API_URL, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: body
+    })
+  }
+
   componentDidMount = async () => {
-    await this.getMessages()
+    this.getMessages()
   }
 
   render() {
     return (
       <div className="App">
         <Toolbar />
-        <MessageList starCallback={this.starCallback} messages={this.state.messages} />
+        <Messages
+          messages={this.state.messages}
+          toggleSelect={this.toggleSelect}
+          toggleStar={this.toggleStar}
+        />
       </div>
     );
   }
