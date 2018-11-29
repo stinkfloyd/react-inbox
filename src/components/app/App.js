bbfde3f4ff17215729d9553788cd3040ad5ae4de
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-11-29 10:26:44 -0700

    "Comments added for portfolio"

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
       messages: []
     }
   }
-
+  // Makes the request to the API. Method is defaulted to GET and Body is null.
   request = async (method = 'GET', body = null) => {
     if (body) body = JSON.stringify(body)
     return await fetch(process.env.REACT_APP_API_URL, {
@@ -24,11 +24,11 @@ export default class App extends Component {
       body: body
     })
   }
-
+  // When app component mounts, get the messages.
   componentDidMount = async () => {
     this.getMessages()
   }
-
+  // Requests the messages from the API then sets the state.
   getMessages = async () => {
     const response = await fetch(`${process.env.REACT_APP_API_URL}`)
     let resJson = await response.json()
@@ -37,11 +37,11 @@ export default class App extends Component {
       messages: resJson
     })
   }
-
+  // Updates the messages with the attached payload.
   updateMessages = async (payload) => {
     await this.request('PATCH', payload)
   }
-
+  // Finds a message in the state, and then toggles the given message's property value to be the opposite value.
   toggleMessageProperty = (message, property) => {
     const index = this.state.messages.indexOf(message)
     this.setState({
@@ -52,7 +52,7 @@ export default class App extends Component {
       ]
     })
   }
-
+  // This function handles the star in the inbox. Keeps the state and the information on the database in sync. The function will call the API to update the starred property with the "star" command and then toggle the state of the message property starred. 
   toggleStar = async (message) => {
     await this.updateMessages({
       "messageIds": [message.id],
@@ -62,11 +62,11 @@ export default class App extends Component {
 
     this.toggleMessageProperty(message, 'starred')
   }
-
+  // This function handles the selection of messages in the inbox. The function will toggle the state of which messages are selected indivually. 
   toggleSelect = async (message) => {
     this.toggleMessageProperty(message, 'selected')
   }
-
+  // This function handles the selection of messages in the inbox via the select-all button. When called, if no messages are previously selected then it will toggle all messages selected, if some are previously selected it will select the rest, and if all messages are selected it will unselect all messages.
   toggleSelectAll = () => {
     const selectedMessages = this.state.messages.filter(message => message.selected)
     const selected = selectedMessages.length !== this.state.messages.length
@@ -76,7 +76,7 @@ export default class App extends Component {
       ))
     })
   }
-
+  // This function handles the read property of selected messages in the inbox. Keeps the state and the information on the database in sync. The function will call the API to update the read property with the "read" command to true and then toggle the state of the selected message property read to true. 
   markRead = async () => {
     await this.updateMessages({
       "messageIds": this.state.messages.filter(message => message.selected).map(message => message.id),
@@ -90,7 +90,7 @@ export default class App extends Component {
       ))
     })
   }
-
+  // This function handles the read property of selected messages in the inbox. Keeps the state and the information on the database in sync. The function will call the API to update the read property with the "read" command to false and then toggle the state of the selected message property read to false. 
   markUnread = async () => {
     await this.updateMessages({
       "messageIds": this.state.messages.filter(message => message.selected).map(message => message.id),
@@ -104,7 +104,7 @@ export default class App extends Component {
       ))
     })
   }
-
+  // Simply deletes the message from the inbox. Uses the API command delete, then updates state.
   deleteMessages = async () => {
     await this.updateMessages({
       "messageIds": this.state.messages.filter(message => message.selected).map(message => message.id),
@@ -114,7 +114,7 @@ export default class App extends Component {
     const messages = this.state.messages.filter(message => !message.selected)
     this.setState({ messages })
   }
-
+  // This handles the add label functionality of the inbox. Updates selected messages with the given label with an API call command "add label", then updates the state.
   addLabel = async (label) => {
     await this.updateMessages({
       "messageIds": this.state.messages.filter(message => message.selected).map(message => message.id),
@@ -129,7 +129,7 @@ export default class App extends Component {
     ))
     this.setState({ messages })
   }
-
+  // This handles the remove label functionality of the inbox. Removes selected label from the given message with an API call command "add label", then updates the state.
   removeLabel = async (label) => {
     await this.updateMessages({
       "messageIds": this.state.messages.filter(message => message.selected).map(message => message.id),
@@ -152,11 +152,11 @@ export default class App extends Component {
     })
     this.setState({ messages })
   }
-
+  // Shows or Hides the compose component.
   toggleCompose = () => {
     this.setState({ composing: !this.state.composing })
   }
-
+  // POST request to API then update state.
   sendMessage = async (message) => {
     const response = await this.request('POST', {
       subject: message.subject,
@@ -170,7 +170,7 @@ export default class App extends Component {
       composing: false,
     })
   }
-
+  // HTML from style guide, with correct updates for assignment.
   render = () => {
     return (
       <div>
@@ -191,6 +191,7 @@ export default class App extends Component {
         <div className="container">
           <Toolbar messages={this.state.messages} markAsRead={this.markRead} markAsUnread={this.markUnread} deleteMessages={this.deleteMessages} toggleSelectAll={this.toggleSelectAll} toggleCompose={this.toggleCompose} applyLabel={this.addLabel} removeLabel={this.removeLabel} />
           {
+            // If composing is true, show component 'ComposeMessage'
             this.state.composing ?
               <ComposeMessage sendMessage={this.sendMessage} /> :
               null
